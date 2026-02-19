4130378814f56d9b4ec50d8ac7081df83985f254
MarcusTurner96 <48020834+MarcusTurner96@users.noreply.github.com>
2026-02-19 16:37:25 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/doGior/doGiorsHadEnough")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/MarcusTurner96/doGiorsHadEnough")
     }
 
     android {

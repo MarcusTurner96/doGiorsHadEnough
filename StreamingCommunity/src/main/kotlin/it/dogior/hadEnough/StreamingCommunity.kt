4d662365a4cd2ca2fab9d90027bf2952f5d23648
GitHub Action <action@github.com>
2026-02-27 18:39:44 +0000

    Automated update of mainUrl from Telegram

--- StreamingCommunity/src/main/kotlin/it/dogior/hadEnough/StreamingCommunity.kt
+++ StreamingCommunity/src/main/kotlin/it/dogior/hadEnough/StreamingCommunity.kt
@@ -48,7 +48,7 @@ class StreamingCommunity(override var lang: String = "it") : MainAPI() {
             "X-Inertia-Version" to inertiaVersion,
             "X-Requested-With" to "XMLHttpRequest",
         ).toMutableMap()
-        val mainUrl = "https://streamingcommunityz.network/"
+        val mainUrl = "https://streamingcommunityz.gives/"
         var name = "StreamingCommunity"
         val TAG = "SCommunity"
     }
